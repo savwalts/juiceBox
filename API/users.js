f7da1c1baff5ea6c97679edc89c92d9983462818
Savannah Walters <savwalts@gmail.com>
2021-02-27 15:01:48 -0600

    api edit

--- API/users.js
+++ API/users.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const usersRouter = express.Router();
 
-// usersRouter.use((req, res, next) => {
-//   console.log("A request is being made to /users");
-
-//   next(); // THIS IS DIFFERENT
-// });
-
 const { getAllUsers } = require('../db');
 
 // UPDATE
